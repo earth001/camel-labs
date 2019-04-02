5a7832e8d1bb802c18e6634d36986dab1d6590e2
Juan Moreno <earth001@gmail.com>
2019-04-02 16:24:01 -0300

    Added solution to camel-cbr exercise

--- 6_EIP_Patterns/camel-cbr/src/main/java/com/redhat/gpte/training/camel/SimpleCBRRoute.java
+++ 6_EIP_Patterns/camel-cbr/src/main/java/com/redhat/gpte/training/camel/SimpleCBRRoute.java
@@ -6,22 +6,35 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class SimpleCBRRoute extends RouteBuilder {
 
-    // @EndpointInject(ref="sourceDirectoryCsv")
-    Endpoint sourceUri;
+  @EndpointInject(ref = "sourceDirectoryCsv")
+  Endpoint sourceUri;
 
-    // @EndpointInject(ref="euroSinkCsv")
-    Endpoint euroSinkUri;
+  @EndpointInject(ref = "euroSinkCsv")
+  Endpoint euroSinkUri;
 
-    // @EndpointInject(ref="usdSinkCsv")
-    Endpoint usdSinkUri;
+  @EndpointInject(ref = "usdSinkCsv")
+  Endpoint usdSinkUri;
 
-    // @EndpointInject(ref="otherSinkCsv")
-    Endpoint otherSinkUri;
+  @EndpointInject(ref = "otherSinkCsv")
+  Endpoint otherSinkUri;
 
-	@Override
-	public void configure() throws Exception {
-
-		from(sourceUri).to("ADD_CBR");
-	}
+  @Override
+  public void configure() throws Exception {
+    // @formatter:off
+    from(sourceUri).log(">> Processing CSV files - ${file:onlyname} <<")
+      .choice()
+        .when()
+          .simple("${file:onlyname} == 'EUPayments.txt'")
+            .log("This is an Euro Payment: ${file:onlyname}")
+            .to(euroSinkUri)
+        .when()
+          .simple("${file:onlyname} == 'USPayments.txt'")
+            .log("This is an USD Payment: ${file:onlyname}")
+            .to(usdSinkUri)
+        .otherwise()
+          .log("This is an Other Currency Payment: ${file:onlyname}")
+          .to(otherSinkUri);
+    // @formatter:on
+  }
 
 }
