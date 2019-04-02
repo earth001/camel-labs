fdac0b7e05c2e6d4dde1d967b17644fb1ca8a60b
Juan Moreno <earth001@gmail.com>
2019-04-02 16:56:43 -0300

    Added solution to camel-recipientlist exercise

--- 6_EIP_Patterns/camel-recipientlist/src/main/java/com/redhat/gpte/training/camel/RecipientRoute.java
+++ 6_EIP_Patterns/camel-recipientlist/src/main/java/com/redhat/gpte/training/camel/RecipientRoute.java
@@ -4,8 +4,16 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class RecipientRoute extends RouteBuilder {
 
-	public void configure() throws Exception {
-
-	}
+  @Override
+  public void configure() throws Exception {
+    //@formatter:off
+    from("timer:recipient?period=5000")
+      .setBody().constant("Say Hello for recipient")
+      .setHeader("recipients").constant("direct:a;direct:b")
+      .recipientList(header("recipients").tokenize(";"));
+    //@formatter:on
+    from("direct:a").log(">> Recipient A called - ${body}");
+    from("direct:b").log(">> Recipient B called - ${body}");
+  }
 
 }
