8f3ea6497b4ee281432377cdf4493c79afc32f71
Juan M.vi <earth001@gmail.com>
2019-04-01 21:52:20 -0300

    Added solution to camel-spring-standalone exercise

--- 4_Camel_Project_Development/camel-spring-standalone/src/main/java/com/redhat/gpte/training/camel/SpringMainApp.java
+++ 4_Camel_Project_Development/camel-spring-standalone/src/main/java/com/redhat/gpte/training/camel/SpringMainApp.java
@@ -6,13 +6,16 @@ import org.slf4j.LoggerFactory;
 
 public class SpringMainApp {
 
-    private static Logger logger = LoggerFactory.getLogger(SpringMainApp.class);
+  private static Logger logger = LoggerFactory.getLogger(SpringMainApp.class);
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        // create a Main instance
-        Main main = new Main();
-
-    }
+    // create a Main instance
+    Main main = new Main();
+    main.enableHangupSupport();
+    main.setApplicationContextUri("META-INF/spring/spring-camel-context.xml");
+    logger.info("Starting SpringMainApp");
+    main.run();
+  }
 
 }
--- 4_Camel_Project_Development/camel-spring-standalone/src/main/java/com/redhat/gpte/training/camel/bean/MyBean.java
+++ 4_Camel_Project_Development/camel-spring-standalone/src/main/java/com/redhat/gpte/training/camel/bean/MyBean.java
@@ -3,5 +3,7 @@ package com.redhat.gpte.training.camel.bean;
 import org.apache.camel.Body;
 
 public class MyBean {
-
+  public String sayHello(@Body String to) {
+    return String.format("Hello : %s", to);
+  }
 }
