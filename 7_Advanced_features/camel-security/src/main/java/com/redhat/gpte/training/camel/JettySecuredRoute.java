0094f839ee34df4572e111731df932637c3d161e
Juan M.vi <earth001@gmail.com>
2019-04-03 01:06:38 -0300

    Added solution to camel-security exercise

--- 7_Advanced_features/camel-security/src/main/java/com/redhat/gpte/training/camel/JettySecuredRoute.java
+++ 7_Advanced_features/camel-security/src/main/java/com/redhat/gpte/training/camel/JettySecuredRoute.java
@@ -5,19 +5,21 @@ import org.apache.camel.main.Main;
 
 public class JettySecuredRoute extends RouteBuilder {
 
-    public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        main.addRouteBuilder(new JettySecuredRoute());
-        // Bind the SecurityHandler with the key myAuthHandler
-        main.enableHangupSupport();
-        main.start();
-    }
+  public static void main(String[] args) throws Exception {
+    Main main = new Main();
+    main.addRouteBuilder(new JettySecuredRoute());
+    // Bind the SecurityHandler with the key myAuthHandler
+    main.bind("myAuthHandler", MySecurityHandler.generate());
+    main.enableHangupSupport();
+    main.start();
+  }
 
-    @Override
-    public void configure() throws Exception {
+  @Override
+  public void configure() throws Exception {
 
     // Add the Jetty Secured Route
+    from("jetty://http://localhost:9191/demo?handlers=myAuthHandler")
+        .transform(constant("<html><body><p>Bye World</p></body></html>"));
 
-
-    }
+  }
 }
